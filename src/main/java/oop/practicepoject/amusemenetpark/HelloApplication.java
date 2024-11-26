7f63bc76c2b585daf5746cfcad49f362f8c8eb39
User <sarderrafidpro@gmail.com>
2024-11-26 22:23:55 +0600

    Rafid Demo1

--- src/main/java/oop/practicepoject/amusemenetpark/HelloApplication.java
+++ src/main/java/oop/practicepoject/amusemenetpark/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ShopMerc.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("ShopMer.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
